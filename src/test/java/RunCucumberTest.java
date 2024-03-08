25aebf1debf9c2bec2a7b9c7e80315d67e72d46f
Seenaa05 <95310387+Seenaa05@users.noreply.github.com>
2024-03-08 09:14:57 +0000

    first tests

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -3,9 +3,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty"}
+        features = "src/test/resources/features",
+        glue = "com.example.sakila", // should be the package containing your step definitions
+        plugin = {"pretty", "html:target/cucumber"}
 )
 public class RunCucumberTest {
-
-
+    // No methods needed, this is just a configuration class
 }
